1b2f9d8f07b0c6b8f26dbc3dcb86aa4c09238d6a
tanveer123 <tanveer_411393@hotmail.com>
2024-02-10 23:28:14 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let Todo = require('./Todo');
 app.use(cors(corsOptions));*/
 
 app.use(cors(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://todo-app-front-fawn.vercel.app"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
