86668d3e8a1a843a073b7e72d6468dbe0a1b44ba
tanveer123 <tanveer_411393@hotmail.com>
2024-02-10 20:21:12 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,41 @@
 const express = require('express');
 const app = express();
-const todoRoutes = express.Router();
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000;
 let Todo = require('./Todo');
 require("dotenv").config();
 
-const corsOptions = {
-    origin: "https://todo-app-front-fawn.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true
-}
+// Allow CORS middleware
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-app.use(cors(corsOptions));
+// Apply CORS middleware to all routes
+app.use(allowCors);
+
+// Body parser middleware
 app.use(bodyParser.json());
-
-// Set up CORS preflight OPTIONS request handling
-app.options('*', cors(corsOptions));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
-})
+});
 
+// Define routes
 app.get('/', async (req, res) => {
     try {
         res.status(201).json({ message: "Connected to Backend!" });
@@ -36,6 +45,7 @@ app.get('/', async (req, res) => {
     }
 });
 
+// Start the server
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
