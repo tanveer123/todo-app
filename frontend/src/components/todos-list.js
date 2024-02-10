24ae2d4c1b756b51617765979802f224a77b9ee2
tanveer123 <tanveer_411393@hotmail.com>
2024-02-10 19:33:23 +0500

    Update todos-list.js

--- frontend/src/components/todos-list.js
+++ frontend/src/components/todos-list.js
@@ -16,14 +16,72 @@ const Todo = props => (
 )
 
 export default class TodosList extends Component {
-	constructor(props) {
+	constructor(props) {import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"https://todo-app-back-murex.vercel.app/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+);
+
+export default class TodosList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { todos: [] };
+    }
+
+    componentDidMount() {
+        fetch('https://todo-app-back-murex.vercel.app/')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ todos: data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    todoList() {
+        return this.state.todos.map((currentTodo, i) => {
+            return <Todo todo={currentTodo} key={i} />;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.todoList()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
         super(props);
         this.state = {todos: []};
     }
 
     componentDidMount() {
 	//axios.defaults.withCredentials = true;
-        /*fetch('https://todo-app-back-murex.vercel.app/')
+        fetch('https://todo-app-back-murex.vercel.app/')
             .then(response => {
                 this.setState({ todos: response.data });
                 //console.log(response.data);
