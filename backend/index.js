3b81228533b5fd19a247ab093229f079cde9ed0f
tanveer123 <tanveer_411393@hotmail.com>
2024-02-15 19:40:13 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 //mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
-mongoose.connect('mongodb+srv://admin:<admin@123>@cluster0.shwdnpa.mongodb.net/?retryWrites=true&w=majority');
+//mongoose.connect('mongodb+srv://admin:<admin@123>@cluster0.shwdnpa.mongodb.net/?retryWrites=true&w=majority');
 /*const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
