074754b87f989f35b381aaf03b5f0d1fd61d89a7
tanveer123 <tanveer_411393@hotmail.com>
2024-02-16 18:47:35 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 
 //mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 mongoose.connect(process.env.MONGODB_URI);
-//const connection = mongoose.connection;
+const connection = mongoose.connection;
 /*connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })*/
@@ -60,7 +60,7 @@ app.get("/", (req, res) => {
 	/*const todos = await Todo.find();
     res.json(todos);
     res.status(201).json({ data: todos });*/
-    res.status(201).json({ message: PORT });
+    res.status(201).json({ message: connection });
 });
 
 /*todoRoutes.route('/:id').get(async function(req, res) {
