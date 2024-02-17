76478b7981803e07d9e6973eb8b4be9d745281fb
tanveer123 <tanveer_411393@hotmail.com>
2024-02-17 19:05:34 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -51,8 +51,8 @@ app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
 
-app.get("/",  (req, res) => {
-	const todos = Todo.find();
+app.get("/",  async (req, res) => {
+	const todos = await Todo.find();
     //res.json(todos);
     //res.status(201).json({ data: process.env.MONGODB_URI });
     res.status(201).json({ message: Todo });
