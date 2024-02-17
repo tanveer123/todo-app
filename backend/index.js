58ea5a0ca7d5cf77291267225433816b7bd9691a
tanveer123 <tanveer_411393@hotmail.com>
2024-02-17 18:45:02 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.listen(PORT, function() {
 
 app.get("/",  (req, res) => {
 	const todos = Todo.find();
-    /*res.json(todos);*/
+    res.json(todos);
     //res.status(201).json({ data: process.env.MONGODB_URI });
     res.status(201).json({ message: "hello" });
 	/*try {
